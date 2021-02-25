89ed005ddfd406f019f5511ae3e319fb590e5968
Abraham Emmanuel <sundayemmanuelabraham@gmail.com>
2021-02-25 13:50:16 +0100

    fix: env setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// List articles
+Route::get('articles', 'ArticleController@index');
+
+//List single article
+Route::get('article/{id}', 'ArticleController@show');
+
+// Create new article article
+Route::post('article', 'ArticleController@store');
+
+// Update article
+Route::put('article', 'ArticleController@store');
+
+// Delete article
+Route::delete('article/{id}', 'ArticleController@destroy');
